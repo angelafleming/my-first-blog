8c594aa973d41591d7be8fb211e9ce3f69fd2de0
ange <ange.fleming@gmail.com>
2017-12-09 16:06:25 +1300

    update files

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,9 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(
+        blank=True, null=True)
+
 
     def publish(self):
         self.published_date = timezone.now()
@@ -16,4 +19,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-    
